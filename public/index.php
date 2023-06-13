f5ff475ed2c322eff3659b3b7c4896b44fff46d3
Florian Blanchet <contact@minzord.fr>
2023-06-13 20:54:44 +0200

    Add comments

--- public/index.php
+++ public/index.php
@@ -7,11 +7,20 @@ require './../vendor/autoload.php';
 
 define('BASE_VIEW_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR);
 
+/**
+ * Initalize the router
+ */
 $router = new Router();
 
+/**
+ * Web routes
+ */
 $router->register('/', ['Controllers\HomeController', 'index']);
 $router->register('/aujourd\'hui', ['Controllers\HomeController', 'today']);
 $router->register('/demain', ['Controllers\HomeController', 'tomorrow']);
 $router->register('/mois', ['Controllers\HomeController', 'month']);
 
+/**
+ * Resolve routes
+ */
 (new App($router, $_SERVER['REQUEST_URI']))->run();
