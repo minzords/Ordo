f9f1421976fc38858a4890a3c52affca9ccd8636
Florian Blanchet <contact@minzord.fr>
2023-06-16 20:41:52 +0200

    Add some tests

--- tests/test.php
+++ tests/test.php
@@ -11,13 +11,44 @@ class CalendariumTest extends TestCase {
      * @covers Calendarium::getCalendarium
      * @return void
      */
-    public function getCalendarium()
-    {
+    public function getCalendarium(){
         $timestart = '2023-06-01';
         $timeend = '2023-06-30';
 
         $instance = new Calendarium();
         $result = $instance->getCalendarium($timestart, $timeend);
+
+        $this->assertIsString($result); // Check if the result is a string
+        $this->assertNotEmpty($result); // Check if the result is not empty
+    }
+
+    /**
+     * Test the getCalendariumByDate method
+     * @test
+     * @covers Calendarium::getCalendariumByDate
+     * @return void
+     */
+    public function getCalendariumByDate(){
+        $date = '2023-06-01';
+
+        $instance = new Calendarium();
+        $result = $instance->getCalendariumByDate($date);
+
+        $this->assertIsString($result); // Check if the result is a string
+        $this->assertNotEmpty($result); // Check if the result is not empty
+    }
+
+    /**
+     * Test the getGenitivus method
+     * @test
+     * @covers Calendarium::getGenitivusByDate
+     * @return void
+     */
+    public function getGenitivusByDate(){
+        $date = '2023-06-01';
+
+        $instance = new Calendarium();
+        $result = $instance->getGenitivusByDate($date);
 
         $this->assertIsString($result); // Check if the result is a string
         $this->assertNotEmpty($result); // Check if the result is not empty
