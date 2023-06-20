c1b3ac6f9e42b4928f6c449283db4ea731646209
Florian Blanchet <contact@minzord.fr>
2023-06-20 15:06:15 +0200

    Add comments for HomeController

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -7,39 +7,74 @@ use Class\Calendarium\Calendarium;
 
 class HomeController
 {
+    /**
+     * Display the home page of the website
+     *
+     * @return Renderer
+     */
     public function index() : Renderer {
         $response = $this->calendariumMonth();
         return Renderer::make('accueil', ['calendarium' => $response]);
     }
 
+    /**
+     * Return the saint/feast of the day in view
+     *
+     * @return string
+     */
     public function today() : string {
         $calendarium = new Calendarium();
         $calendarium = $calendarium->getGenitivusByDate(self::datetoday());
         return $calendarium;
     }
 
+    /**
+     * Return the saint/feast of tomorrow in view
+     *
+     * @return string
+     */
     public function tomorrow() : string {
         $calendarium = new Calendarium();
         $calendarium = $calendarium->getGenitivusByDate(self::datetomorrow());
         return $calendarium;
     }
 
+    /**
+     * Return the calendarium of the month
+     *
+     * @return array
+     */
     public function month() : array{
         $response = $this->calendariumMonth();
         return dd($response);
     }
 
+    /**
+     * Return the saint/feast of the day
+     *
+     * @return string
+     */
     private function datetoday() : string {
         $currentDate = date('Y-m-d');
         return $currentDate;
     }
 
+    /**
+     * Return the calendarium of tomorrow
+     *
+     * @return string
+     */
     private function dateTomorrow() : string {
         $currentDate = date('Y-m-d');
         $tomorrow = date('Y-m-d', strtotime($currentDate . ' + 1 days'));
         return $tomorrow;
     }
 
+    /**
+     * Return the calendarium of the month
+     *
+     * @return array
+     */
     private function calendariumMonth() : array {
         $today = date('Y-m-d');
         $lastDayOfMonth = date('Y-m-t');
